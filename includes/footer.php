cabbe71e27470f5b81c975021e37c2b3637bde5a
Aman Jain <71557159+amanj7820@users.noreply.github.com>
2021-06-26 12:17:24 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,4 @@
+
+<footer class="main-footer" style="text-align:center">
+        <strong>Copyright &copy; 2021</strong> Brought To You By <a href="https://www.campcodes.com">https://www.campcodes.com</a>
+</footer>
